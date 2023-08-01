7d28adb6282aaf056078fe788a13aebf6dac87ce
EC2 Default User <ec2-user@ip-172-31-31-207.ap-northeast-1.compute.internal>
2023-08-01 11:38:34 +0000

    finish

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,5 +10,17 @@ class Book < ApplicationRecord
   def faborited_by?(user)
    faborites.exists?(user_id: user.id)
   end
+  
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      Book.where('name LIKE ?', '%' + content)
+    else
+      Book.where('name LIKE ?', '%' + content + '%')
+    end
+  end
  
 end
